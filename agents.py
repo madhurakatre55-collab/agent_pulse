3b15a919cf967dbc6f24d165827353cf5e37a56a
Madhura Katre <madhurakatre55@gmail.com>
2026-07-28 00:32:13 +0530

    created agents.py

---
+++ agents.py
@@ -0,0 +1,17 @@
+from langchain.agents import create_agent 
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptsTemplate
+from langchain_core.output_parsers import StrOutputParser
+from tools import web_serch, scrape_url
+from dotenv import
+
+load_dotenv()
+
+llm = ChatOpenAI(model = "",temperature=0)
+
+def build_search_agents():
+    return create_agent(
+        model = llm,
+        
+        
+    )
