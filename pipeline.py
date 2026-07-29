524f3b9e28fa772ce93bcc77d09f3554d80b092b
Madhura Katre <madhurakatre55@gmail.com>
2026-07-29 21:26:23 +0530

    new changes

--- pipeline.py
+++ pipeline.py
@@ -3,4 +3,10 @@ from agents import build_reader_agent, build_search_agent,writer_chain,critic_ch
 def run_research_pipeline(topic: str) -> dict:
     state = {}
     #seacrh agent working 
+    search_agent = build_search_agent()
+    search_result =search_agent.invoke({
+        "messages" : [("user",f"Find recent,reliable and detailed information about:{topic}" )]
+    })
+    state["search_result"] = search_result['messages'][-1].ContentDispositionHeader
+    
     
